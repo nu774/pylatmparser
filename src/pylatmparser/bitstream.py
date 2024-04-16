45adef395c964c15e79c350e9ec5f124dd4bb846
nu774 <honeycomb77@gmail.com>
2024-04-16 22:01:14 +0900

    use flac_bitstream for performance

--- src/pylatmparser/bitstream.py
+++ src/pylatmparser/bitstream.py
@@ -1,60 +1,6 @@
-from collections.abc import ByteString
-from bitarray import bitarray
-from bitarray.util import ba2int, int2ba
+try:
+    from .bitstream_flac import BitReader, BitWriter
+except:
+    from .bitstream_bitarray import BitReader, BitWriter
 
 __all__ = [ 'BitReader', 'BitWriter' ]
-
-class BitReader:
-    def __init__(self, data: ByteString):
-        self.bits = bitarray()
-        self.bits.frombytes(data)
-        self.pos = 0
-    
-    def read(self, nbits: int) -> int:
-        value = ba2int(self.bits[self.pos:self.pos+nbits])
-        self.pos += nbits
-        return value
-
-    def tell(self) -> int:
-        return self.pos
-    
-    def skip(self, len: int) -> None:
-        self.pos += len
-    
-    def byte_align(self) -> None:
-        self.pos = ((self.pos + 7) // 8) * 8
-    
-    def read_bytes(self, nbits: int) -> bytes:
-        value = self.bits[self.pos:self.pos+nbits].tobytes()
-        self.pos += nbits
-        return value
-    
-    def tobytes(self) -> bytes:
-        return self.bits[self.pos:].tobytes()
-
-    def latm_get_value(self) -> int:
-        bytes_for_value = self.read(2)
-        value = 0
-        for _ in range(bytes_for_value):
-            value <<= 8
-            value_tmp = self.read(8)
-            value |= value_tmp
-        return value
-
-class BitWriter:
-    def __init__(self):
-        self.bits = bitarray()
-    
-    def write(self, value: int, nbits: int) -> None:
-        self.bits.extend(int2ba(value, length=nbits))
-
-    def byte_align(self) -> None:
-        pos = ((len(self.bits) + 7) // 8) * 8
-        while len(self.bits) < pos:
-            self.bits.append(0)
-    
-    def write_bytes(self, data: ByteString) -> None:
-        self.bits.frombytes(data)
-    
-    def tobytes(self) -> bytes:
-        return self.bits.tobytes()
---
+++ src/pylatmparser/bitstream_flac.py
@@ -0,0 +1,65 @@
+from collections.abc import ByteString
+from flac_bitstream import BitReader as FLAC_BitReader
+from flac_bitstream import BitWriter as FLAC_BitWriter
+import sys
+
+__all__ = [ 'BitReader', 'BitWriter' ]
+
+class BitReader:
+    def __init__(self, data: ByteString):
+        self.data = bytes(data[:])
+        self.bits = FLAC_BitReader(self.data)
+    
+    def read(self, nbits: int) -> int:
+        return self.bits.read_bits(nbits)
+
+    def tell(self) -> int:
+        return len(self.data) * 8 - self.bits.get_input_bits_unconsumed()
+    
+    def skip(self, len: int) -> None:
+        if len > 0:
+            self.bits.skip_bits(len)
+    
+    def byte_align(self) -> None:
+        n = self.bits.bits_left_for_byte_alignment()
+        self.bits.skip_bits(n)
+    
+    def read_bytes(self, nbits: int) -> bytes:
+        if nbits > 0:
+            buf = bytearray((nbits + 7) // 8)
+            self.bits.read_byte_block(buf)
+        return buf
+    
+    def tobytes(self) -> bytes:
+        bits = FLAC_BitReader(self.data + b'\0')
+        n = self.tell()
+        bits.skip_bits(n)
+        ba = bytearray(len(self.data) - n // 8)
+        bits.read_byte_block(ba)
+        return ba
+
+    def latm_get_value(self) -> int:
+        bytes_for_value = self.read(2)
+        value = 0
+        for _ in range(bytes_for_value):
+            value <<= 8
+            value_tmp = self.read(8)
+            value |= value_tmp
+        return value
+
+class BitWriter:
+    def __init__(self):
+        self.bits = FLAC_BitWriter()
+    
+    def write(self, value: int, nbits: int) -> None:
+        self.bits.write_bits(value, nbits)
+
+    def byte_align(self) -> None:
+        self.bits.zero_pad_to_byte_boundary()
+    
+    def write_bytes(self, data: ByteString) -> None:
+        self.bits.write_byte_block(data)
+    
+    def tobytes(self) -> bytes:
+        self.bits.zero_pad_to_byte_boundary()
+        return self.bits.get_buffer()
